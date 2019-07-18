2b53d4692638055a6af3bac247e424095adaedc6
wal2323 <radgeyin@yahoo.com>
2019-07-18 15:35:53 +0100

    add touch for mobile

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ let level = 0;
 
 //Start game with keyboard press
 
-$(document).keypress(function() {
+$(document).on('keypress touch'(function() {
   if (!started){
     $('#level-title').text('Level ' + level);
     nextSequence();
